424d8f062d20dd05f008d6f548d8179ef806b149
Sidharth Verma <sidhartv@andrew.cmu.edu>
2016-02-06 04:38:45 -0500

    Wrote: ability to receive strings from client and create an order

--- TeamPong/src/edu/cmu/teampong/gameserver/MainServer.java
+++ TeamPong/src/edu/cmu/teampong/gameserver/MainServer.java
@@ -1,6 +1,9 @@
 package edu.cmu.teampong.gameserver;
 
+import java.io.BufferedReader;
+import java.io.DataInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -19,6 +22,19 @@ public class MainServer {
 			} catch (IOException e) {
 				System.err.println("Failed to accept socket connection");
 			}
+			BufferedReader reader = null;
+			try {
+				reader = new BufferedReader(new InputStreamReader(clientSock.getInputStream()));
+			} catch (IOException e) {
+				System.err.println("Failed to generate buffered reader");
+			}
+			String readString = null;
+			try {
+				readString = reader.readLine();
+			} catch (IOException e) {
+				System.err.println("Failed to read in bytes from source socket");
+			}
+			Order order = Order.parse(readString);
 			
 			
 		}
